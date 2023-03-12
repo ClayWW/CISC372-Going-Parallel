4fe961742f03380b2eb6a3d0d0fc7dc76fa05185
Clay <clayw@udel.edu>
2023-03-12 18:51:21 -0400

    changed casting

--- main.c
+++ main.c
@@ -177,7 +177,7 @@ int main(int argc,char** argv){
         if (isStraightFlush(pokerHand))
             straightFlushes++;
     }
-    MPI_Reduce(straightFlushes, totalStraights, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&straightFlushes, &totalStraights, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     if(my_rank == 0){
         percent=(float)totalStraights/(float)cnt*100.0;
         printf("We found %d straight flushes out of %d hands or %f percent.\n",totalStraights,cnt,percent);
